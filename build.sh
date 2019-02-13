0a1c9290cd8f6402add9c211bbfc0eb4d4c148e3
bbelovic <bbelovic@gmail.com>
2019-02-13 21:23:29 +0100

    specifying main class in initial module

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 javac --module-source-path "./*/src/main/java" -d classes/ --module cipher.cli
+
+rm -rf mods/*
+
+jar --create --file mods/cipher.cli.jar --main-class net.bbelovic.cipher.cli.Main -C classes/cipher.cli .
+jar --create --file mods/cipher.jar -C classes/cipher .
