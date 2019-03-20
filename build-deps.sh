19dd68b50b850e5151319e74eb0991b8907747e0
bbelovic <bbelovic@gmail.com>
2019-03-20 22:14:42 +0100

    created multi release jar

--- build-deps.sh
+++ build-deps.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 #javac --release 8 -d classes $(find dep2/src/main/java -name '*.java')
-javac --release 9 -d classes-9 $(find dep2/src/main/java -name '*.java')
+#javac --release 9 -d classes-9 $(find dep2/src/main/java -name '*.java')
+jar --create --file mr.jar -C classes . --release 9 classes-9/module-info.class
 #javac --class-path classes -d classes $(find dep1/src/main/java -name '*.java')
