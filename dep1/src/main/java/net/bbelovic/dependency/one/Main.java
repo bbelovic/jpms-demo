144ec9bdd312343cafa281f5bc75dbd7543588d6
bbelovic <bbelovic@gmail.com>
2019-04-07 22:29:53 +0200

    Filtering service provider by annotation

--- dep1/src/main/java/net/bbelovic/dependency/one/Main.java
+++ dep1/src/main/java/net/bbelovic/dependency/one/Main.java
@@ -1,6 +1,7 @@
 package net.bbelovic.dependency.one;
 
 import net.bbelovic.services.SimpleService;
+import net.bbelovic.services.annotation.Alpha;
 
 import java.util.List;
 import java.util.ServiceLoader;
@@ -10,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
         List<SimpleService> services = ServiceLoader.load(SimpleService.class)
                 .stream()
+                .filter(provider -> provider.type().isAnnotationPresent(Alpha.class))
                 .map(ServiceLoader.Provider::get)
                 .collect(Collectors.toList());
         System.out.println("Found services: "+ services.size());
--- dep3/src/main/java/net/bbelovic/dependency/three/AnotherSimpleService.java
+++ dep3/src/main/java/net/bbelovic/dependency/three/AnotherSimpleService.java
@@ -1,7 +1,9 @@
 package net.bbelovic.dependency.three;
 
 import net.bbelovic.services.SimpleService;
+import net.bbelovic.services.annotation.Alpha;
 
+@Alpha
 public class AnotherSimpleService implements SimpleService {
     @Override
     public void execute() {
