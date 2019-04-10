b2f8e82c30dbda7abe35f4f7d8035228c3a6b829
bbelovic <bbelovic@gmail.com>
2019-04-10 21:46:54 +0200

    tweaked buildscript

--- build-ch11.sh
+++ build-ch11.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 rm -rf classes/*
 javac -d classes $(find chapter11-dep2/src/main/java -name '*.java')
-jar --create --file mods/chapter11-dep2.jar $(cd classes; find . -path "*/exposed/*") classes/module-info.class
-
-#javac -p mods/ -d classes $(find chapter11-dep1/src/main/java -name '*.java')
-#jar --create --file mods/chapter11-dep1.jar $(find classes -path "*/exposing/*") classes/module-info.class
+cd classes
+jar --create --file ../mods/chapter11-dep2.jar $(find . -path "*/exposed/*") ./module-info.class
+cd ..
+javac -p mods/ -d classes $(find chapter11-dep1/src/main/java -name '*.java')
+cd classes
+jar --create --file ../mods/chapter11-dep1.jar $(find . -path "*/exposing/*") ./module-info.class
