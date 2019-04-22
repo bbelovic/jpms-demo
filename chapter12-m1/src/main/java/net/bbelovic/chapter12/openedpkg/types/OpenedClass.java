d0a3c10e73cbd3a6041bfe5b0d8ea107f7c9726a
bbelovic <bbelovic@gmail.com>
2019-04-22 08:13:49 +0200

    exercise: reflection and open packages

--- chapter12-m1/src/main/java/net/bbelovic/chapter12/openedpkg/types/OpenedClass.java
+++ chapter12-m1/src/main/java/net/bbelovic/chapter12/openedpkg/types/OpenedClass.java
@@ -1,9 +1,22 @@
 package net.bbelovic.chapter12.openedpkg.types;
 
 public class OpenedClass {
-    private final Object deepField = new Object();
+    private int id = 12;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public void execute() {
         System.out.println("hello from opened class!");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("OpenedClass{id=%d}", id);
     }
 }
--- chapter12-m2/src/main/java/net/bbelovic/chapter12/reflection/reader/main/Main.java
+++ chapter12-m2/src/main/java/net/bbelovic/chapter12/reflection/reader/main/Main.java
@@ -2,10 +2,31 @@ package net.bbelovic.chapter12.reflection.reader.main;
 
 import net.bbelovic.chapter12.exportedpkg.types.ExportedClass;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 public class Main {
     public static void main(String[] args) {
+        exportsDoesNotSupportDeepReflection();
+    }
+
+    private static void opensSupportsDeepReflection() {
+        try {
+            var openedCls = Class.forName("net.bbelovic.chapter12.openedpkg.types.OpenedClass");
+            var instance = openedCls.getConstructor().newInstance();
+            System.out.println("instance of OpenedClass created: "+ instance);
+
+            Field id = instance.getClass().getDeclaredField("id");
+            id.setAccessible(true);
+            id.set();
+
+
+        } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void exportsDoesNotSupportDeepReflection() {
         try {
             var constructor = ExportedClass.class.getConstructor();
             var instance = constructor.newInstance();
