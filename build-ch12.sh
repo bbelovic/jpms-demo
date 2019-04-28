833a3244d7e498baa24f1f8606c889e787405ee9
bbelovic <bbelovic@gmail.com>
2019-04-28 21:40:32 +0200

    first version of reflection via varhandles

--- build-ch12.sh
+++ build-ch12.sh
@@ -6,7 +6,14 @@ cd classes
 jar -c -f ../mods/chapter12-m1.jar $(find . -path "*/chapter12/*") ./module-info.class
 
 cd ..
+javac -d classes $(find chapter12-impl/src/main/java -name '*.java')
+
+cd classes
+jar -c -f ../mods/chapter12-impl.jar $(find . -path "*/impl/*") ./module-info.class
+
+cd ..
 javac -p mods/ -d classes $(find chapter12-m2/src/main/java -name '*.java')
 
 cd classes
 jar -c -f ../mods/chapter12-m2.jar $(find . -path "*/reader/*") ./module-info.class
+
